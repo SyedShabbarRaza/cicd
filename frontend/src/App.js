3ae086a41f356c2e1bb5340a1e18b7e18663b6bb
Syed Shabbar Raza <syedshabbarraza208@gmail.com>
2026-04-24 16:09:58 +0500

    Update header text in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>CI/CD Piplelines with github Actions.</h1>
+      <h1>CI/CD Piplelines with github Actions are here.</h1>
      </div>
   );
 }
